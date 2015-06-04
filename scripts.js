7400d3ed6ccca88cd51a2734d14934b99bdc9b10
sheldonmike <sheldonmike@gmail.com>
2015-06-04 09:20:52 -0400

    Added Twitter feeds and star effect to Twitter section

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
-// alert("Hello!");
+$(function () {
+    $(window).scroll(function () {
+        var currentScrollTop = $(window).scrollTop();
+        $('#flare').css('opacity',currentScrollTop/($('#flare').height()+900));
+
+    });
+});
+
+// $(function () {
+//     $(window).scroll(function () {
+//         var currentScrollTop = $(window).scrollTop();
+//         $('#flare').css('opacity',currentScrollTop/($('#flare').height()+500));
+
+//     });
+// });
+
+
+// Twitter Tweets
+
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));
