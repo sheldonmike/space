bb27ca6ba73bb8d51e32d5f5b7cf8ef93d09552a
sheldonmike <sheldonmike@gmail.com>
2015-06-08 00:00:49 -0400

    Added Galaxy page and other tweaks
    Added Galaxy.html and galaxy.css.  Also tweaked stars.css to decrease
    star size.  Removed radial-gradient from star section on index.html.
    Added JavaScript to scripts.js for Galaxy.html page.

--- scripts.js
+++ scripts.js
@@ -33,3 +33,69 @@ window.twttr = (function(d, s, id) {
  
   return t;
 }(document, "script", "twitter-wjs"));
+
+
+
+
+
+
+$(document).ready(function(){
+    $(".mercury-button").hover(function(){
+        $('.mercury').css("-webkit-animation-play-state", "paused");
+        $('.mercury-info').removeClass("no-info");
+    }, function(){
+        $('.mercury').css("-webkit-animation-play-state", "running");
+        $('.mercury-info').addClass("no-info");
+    });
+});
+
+$(document).ready(function(){
+    $(".venus-button").hover(function(){
+        $('.venus').css("-webkit-animation-play-state", "paused");
+        $('.venus-info').removeClass("no-info");
+    }, function(){
+        $('.venus').css("-webkit-animation-play-state", "running");
+        $('.venus-info').addClass("no-info");
+    });
+});
+
+$(document).ready(function(){
+    $(".earth-button").hover(function(){
+        $('.earth').css("-webkit-animation-play-state", "paused");
+        $('.earth').css("box-shadow", "0px 0px 50px 1px white");
+        $('.earth-info').removeClass("no-info");
+    }, function(){
+        $('.earth').css("-webkit-animation-play-state", "running");
+        $('.earth').css("box-shadow", "none");
+        $('.earth-info').addClass("no-info");
+    });
+});
+
+$(document).ready(function(){
+    $(".mars-button").hover(function(){
+        $('.mars').css("-webkit-animation-play-state", "paused");
+        $(".mars-orbit").css("border", "1px solid rgba(255,255,255,.3)");
+        $('.mars-info').removeClass("no-info");
+    }, function(){
+        $('.mars').css("-webkit-animation-play-state", "running");
+        $(".mars-orbit").css("border", "none");
+        $('.mars-info').addClass("no-info");
+    });
+});
+
+
+
+
+$(document).ready(function(){
+    $(".rings").hover(function(){
+        $(".mercury-orbit").css("border", "1px solid rgba(255,255,255,.3)");
+        $(".venus-orbit").css("border", "1px solid rgba(255,255,255,.3)");
+        $(".earth-orbit").css("border", "1px solid rgba(255,255,255,.3)");
+        $(".mars-orbit").css("border", "1px solid rgba(255,255,255,.3)");
+    }, function() {
+        $(".mercury-orbit").css("border", "none");
+        $(".venus-orbit").css("border", "none");
+        $(".earth-orbit").css("border", "none");
+        $(".mars-orbit").css("border", "none");
+    });
+});
